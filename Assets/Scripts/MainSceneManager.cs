347e25503484501b55e066520c1270c288114101
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-05-09 23:42:57 +0900

    QuizPanel이 올라가있는동안 플레이어가 움직이지 못하도록 수정

--- Assets/Scripts/MainSceneManager.cs
+++ Assets/Scripts/MainSceneManager.cs
@@ -7,8 +7,7 @@ public class MainSceneManager : MonoBehaviour
     static public MainSceneManager Instance = null;
     //메인 씬 안에서만 존재하는 데이터들을 관리해주는 매니저입니다.
     [SerializeField] GameObject player;
-
-
+ 
     public GameObject Player { get { return player; } private set { player = value; } }
 
     private void Awake()
