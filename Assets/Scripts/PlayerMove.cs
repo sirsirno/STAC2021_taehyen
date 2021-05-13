ba85b8057fabec52e05eaa361c74e96d7fcb9043
DESKTOP-OHD6DA9\USER <zelodoc08@gmail.com>
2021-05-14 01:59:42 +0900

    플레이어 공격 미완

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 public class PlayerMove : MonoBehaviour
 {
     public bool bCanMove = true;
@@ -13,7 +13,11 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]
     private Transform bottomChk;
     [SerializeField]
-    private float movespeed=10,bottomchkDistance=0.1f,jumpPower,velocityY;
+    private float movespeed=10,bottomchkDistance=0.1f,jumpPower,velocityY,coolTime=0f,bCanMoveTime=0f;
+    [SerializeField]
+    private Transform attackPos;
+    [SerializeField]
+    private Vector2 boxsize;
     void Start()
     {
         animator=GetComponent<Animator>();
@@ -23,10 +27,22 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
+        if(coolTime>0)
+            coolTime-=Time.deltaTime;
+        if(bCanMoveTime>0)
+            bCanMoveTime-=Time.deltaTime;
         velocityY=rigid.velocity.y;
         animator.SetFloat("velocityY",velocityY);
         Move();
         BottomChk();
+        if(Input.GetMouseButtonDown(0)){
+            if(!EventSystem.current.IsPointerOverGameObject()&&coolTime<=0f){
+                animator.SetTrigger("attack");
+                coolTime=0.5f;
+                bCanMoveTime=0.5f;
+                rigid.velocity=Vector2.zero;
+            }
+        }
     }
     void BottomChk(){
         Debug.DrawRay(bottomChk.position, Vector3.down * bottomchkDistance, new Color(0, 1, 0));
@@ -35,6 +51,8 @@ public class PlayerMove : MonoBehaviour
     }
     void Move(){
         if (bCanMove == false)
+            return;
+        if (bCanMoveTime > 0f)
             return;
 
         float inputX = Input.GetAxis("Horizontal");
@@ -49,5 +67,20 @@ public class PlayerMove : MonoBehaviour
             animator.SetBool("run",true);
         }else animator.SetBool("run",false);
         rigid.velocity=new Vector3(movespeed * inputX,rigid.velocity.y,0f);
+    }
+    void Attack(){
+        Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(attackPos.position, boxsize, 0);
+                foreach (Collider2D collider in collider2Ds)
+                {
+                    if (collider.tag == "Enemy")
+                    {
+                        Debug.Log("hit!");
+                    }
+                }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(attackPos.position, boxsize);
     }
 }
